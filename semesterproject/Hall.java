1bcd5623e6015e7e17d06bc84d9ad63eec082212
philip <philip@10.200.126.41>
2015-11-16 13:21:53 +0100

    To dimensionelt array, hvis værdier bestemmes ud fra to int parametre.

---
+++ semesterproject/Hall.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semesterproject;
+
+/**
+ *
+ * @author philip
+ */
+public class Hall {
+    private final String name;
+    private final Seat[][] seats;
+  
+
+    public Hall(String name, int numberRows, int numberSeats) {
+        this.name = name;
+        this.seats = new Seat[numberRows][numberSeats];
+    }
+    
+    
+
+    
+    
+    
+    
+    
+}
